7fb8ce8a7d4d62b28c6b7108b2bbcbe5e6c7b956
jr-atabay <152003124+jr-atabay@users.noreply.github.com>
2024-04-11 00:23:58 +0300

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int num, line;
+	char simbol;
+	int a = 0;
+
+	cout << "Введите количесто симбола: " << endl;
+	cin >> num;
+
+	cout << "Ведите симбол: " << endl;
+	cin >> simbol;
+
+	cout << "Выберите тип линия: " << endl
+		<< "1 - вертикальная " << endl
+		<< "2 - горизоньтальная " << endl;
+	cin >> line;
+
+	while (a<num)
+	{
+		if (line == 1)
+		{
+			cout << simbol << endl;
+		}
+		if (line == 2)
+		{
+			cout << simbol;
+		}
+		a++;
+	}
+}
+
+
